dfbfa03e07aabc9718e7e3f2b08be105c6012df0
andrewdanieltodd@gmail.com <andrewdanieltodd@gmail.com>
2024-11-15 18:02:17 -0700

    Version one

--- include/DirectX12Renderer.h
+++ include/DirectX12Renderer.h
@@ -1,14 +1,15 @@
 #ifndef ULTREALITY_RENDERING_DIRECTX12_RENDERER_H
 #define ULTREALITY_RENDERING_DIRECTX12_RENDERER_H
 
-#include <RendererInterface.h>
-
 #include <windows.h>
 #include <wrl.h>
 #include <d3d12.h>
 #include <dxgi1_4.h>
 
 #include <stdint.h>
+
+#include <RendererInterface.h>
+#include <DisplayTarget.h>
 
 #if defined(__GNUC__) or defined(__clang__)
 #define FORCE_INLINE inline __attribute__((always_inline))
@@ -20,39 +21,157 @@
 
 namespace UltReality::Rendering
 {
+	/// <summary>
+	/// Class implements the <seealso cref="UltReality.Rendering.RendererInterface"/> interface using DirectX12
+	/// </summary>
 	class DirectX12Renderer : public RendererInterface
 	{
 	private:
+		// Windows specific handle to a Windows window instance for this application. Render target for DirectX
+		HWND m_mainWin = nullptr;
+
+		// Render Target View descriptor size
+		uint32_t m_rtvDescriptorSize;
+		// Depth Stencil View descriptor size
+		uint32_t m_dsvDescriptorSize;
+		// 
+		uint32_t m_cbvSrvDescriptorSize;
+
+		// If MSAA is found to be supported this is set to true. Used to configure DirectX device to use MSAA
+		bool m_MsaaState = false;
+		// Hardware is queried and quality level set according to capability
+		uint32_t m_MsaaQuality;
+
+		// Variable keeps track of the target window's width in pixels
+		uint32_t m_clientWidth = 1920;
+		// Variable keeps track of the target window's height in pixels
+		uint32_t m_clientHeight = 1080;
+		// Tracks the devices refresh rate
+		DXGI_RATIONAL m_clientRefreshRate = { 60, 1 };
+		
+		// Format of the texels in the swap chain (back buffer)
+		DXGI_FORMAT m_backBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+		// Static value for the number of render targets in our swap chain (2 for double buffering)
+		static constexpr uint8_t m_swapChainBufferCount = 2;
+		// Variable tracks which index in the swap chain we are currently rendering to (back buffer)
+		uint8_t m_currBackBuffer = 0;
+
+		// Format of the texels in the depth stencil buffers
+		DXGI_FORMAT m_depthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+		// ComPtr to the DirectX device
 		Microsoft::WRL::ComPtr<ID3D12Device> m_d3dDevice;
+		// ComPtr to a DXGI Factory object. Used to create DirectX objects and structs
 		Microsoft::WRL::ComPtr<IDXGIFactory4> m_dxgiFactory;
+		// ComPtr to Pipeline fence object
 		Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+		// ComPtr to the renderer's command queue. Items submitted to command queue are submitted as soon as possible
 		Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
+		// ComPtr to an allocator used to create commands for the command list
 		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_directCmdListAlloc;
+		// ComPtr to renderer's command list. Items submitted to command list are pooled until explicitly executed. Can be used to create reusable sequence of commands for repeated submission
 		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+		// ComPtr to the renderer's swap chain. Corresponds to buffer on hardware (device)
+		Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
+		// ComPtr to descriptors for render target views in the swap chain
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+		// ComPtr to descriptor for the depth stencil view
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_dsvHeap;
+		// Array of ComPtr to the render target buffers in the swap chain
+		Microsoft::WRL::ComPtr<ID3D12Resource> m_swapChainBuffer[m_swapChainBufferCount];
+		// ComPtr to the depth stencil view buffer
+		Microsoft::WRL::ComPtr<ID3D12Resource> m_depthStencilBuffer;
 
 #if defined(_DEBUG) or defined(DEBUG)
 		Microsoft::WRL::ComPtr<ID3D12Debug> m_debugController;
 #endif
-
-		uint32_t m_rtvDescriptorSize;
-		uint32_t m_dsvDescriptorSize;
-		uint32_t m_cbvSrvDescriptorSize;
-
-		uint32_t m_4xMsaaQuality;
-
+		/// <summary>
+		/// Method creates the DirectX device <seealso cref="m_d3dDevice"/>
+		/// </summary>
 		FORCE_INLINE void CreateDevice();
+
+		/// <summary>
+		/// Method creates the fence object <seealso cref="m_fence"/>
+		/// </summary>
 		FORCE_INLINE void CreateFence();
+
+		/// <summary>
+		/// Queries the descriptor sizes and sets the descriptor variables. <seealso cref="m_rtvDescriptorSize"/> <seealso cref="m_dsvDescriptorSize"/> <seealso cref="m_cbvSrvDescriptorSize"/>
+		/// </summary>
 		FORCE_INLINE void GetDescriptorSizes();
+
+		/// <summary>
+		/// Queries the devices MSAA quality level support and sets variable with value <seealso cref="m_4xMsaaQuality"/>
+		/// </summary>
+		/// <param name="sampleCount">Quality level to query for</param>
 		FORCE_INLINE void CheckMSAAQualitySupport(const uint32_t sampleCount);
+
+		/// <summary>
+		/// Creates the renderer's command queue and sets <seealso cref="m_commandQueue"/>
+		/// </summary>
 		FORCE_INLINE void CreateCommandQueue();
+
+		/// <summary>
+		/// Creates the command allocators and sets <seealso cref="m_directCmdListAlloc"/>
+		/// </summary>
 		FORCE_INLINE void CreateCommandAllocator();
+
+		/// <summary>
+		/// Creates the command list and sets <seealso cref="m_commandList"/>
+		/// </summary>
 		FORCE_INLINE void CreateCommandList();
 
+		/// <summary>
+		/// Creates the swap chain and sets <seealso cref="m_swapChain"/>
+		/// </summary>
+		FORCE_INLINE void CreateSwapChain();
+
+		/// <summary>
+		/// Creates the descriptor heaps, setting <seealso cref="m_rtvHeap"/>, <seealso cref="m_dsvHeap"/>
+		/// </summary>
+		FORCE_INLINE void CreateDescriptorHeaps();
+
+		/// <summary>
+		/// Initializes the items in <seealso cref="m_swapChainBuffer"/>
+		/// </summary>
+		FORCE_INLINE void CreateRenderTargetView();
+
+		/// <summary>
+		/// Initializes the <seealso cref="m_depthStencilBuffer"/>
+		/// </summary>
+		FORCE_INLINE void CreateDepthStencilBuffer();
+
+		/// <summary>
+		/// Issues command to initialize the viewport and its configuration
+		/// </summary>
+		FORCE_INLINE void SetViewport();
+		//FORCE_INLINE void SetScissorRectangles(D3D12_RECT* rect);
+
+		/// <summary>
+		/// Gets the current back buffer view
+		/// </summary>
+		/// <returns>Handle to the current buffer in the swap chain (back buffer)</returns>
+		FORCE_INLINE D3D12_CPU_DESCRIPTOR_HANDLE CurrentBackBufferView() const;
+
+		/// <summary>
+		/// Gets the depth stencil view buffer
+		/// </summary>
+		/// <returns>Handle to the depth stencil view buffer</returns>
+		FORCE_INLINE D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView() const;
+
 	public:
-		bool RENDERER_INTERFACE_CALL Initialize() final;
-		void RENDERER_INTERFACE_CALL CreateBuffer() final;
-		void RENDERER_INTERFACE_CALL Render() final;
-		void RENDERER_INTERFACE_CALL Present() final;
+		/// <summary>
+		/// Called to initialize the renderer and prepare it for rendering tasks
+		/// </summary>
+		/// <param name="targetWindow">The window for the renderer to target. For DirectX12Renderer this should have been initialized with an HWND instance</param>
+		RENDERER_INTERFACE_ABI void RENDERER_INTERFACE_CALL Initialize(DisplayTarget targetWindow) final;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		RENDERER_INTERFACE_ABI void RENDERER_INTERFACE_CALL CreateBuffer() final;
+		RENDERER_INTERFACE_ABI void RENDERER_INTERFACE_CALL Render() final;
+		RENDERER_INTERFACE_ABI void RENDERER_INTERFACE_CALL Present() final;
 	};
 }
 
--- src/DirectX12Renderer.cpp
+++ src/DirectX12Renderer.cpp
@@ -1,7 +1,9 @@
-#include <DirectX12Renderer.h>
+#include <directx/d3dx12.h>
 
 #include <stdexcept>
 #include <comdef.h>
+
+#include <DirectX12Renderer.h>
 
 using namespace Microsoft::WRL;
 
@@ -84,10 +86,11 @@ namespace UltReality::Rendering
 			sizeof(qualityLevels)
 		));
 
-		m_4xMsaaQuality = qualityLevels.NumQualityLevels;
+		m_MsaaQuality = qualityLevels.NumQualityLevels;
 #if defined(_DEBUG) or defined(DEBUG)
-		assert(m_4xMsaaQuality > 0 && "Unexpected MSAA quality level");
+		assert(m_MsaaQuality > 0 && "Unexpected MSAA quality level");
 #endif
+		m_MsaaState = true;
 	}
 
 	FORCE_INLINE void DirectX12Renderer::CreateCommandQueue()
@@ -123,5 +126,183 @@ namespace UltReality::Rendering
 		// refer to the command list we will reset it, and it needs to be
 		// closed before calling reset
 		m_commandList->Close();
+	}
+
+	FORCE_INLINE void DirectX12Renderer::CreateSwapChain()
+	{
+		// Release the previous swapchain we will be recreating
+		m_swapChain.Reset();
+
+		DXGI_SWAP_CHAIN_DESC sd;
+		sd.BufferDesc.Width = m_clientWidth;
+		sd.BufferDesc.Height = m_clientHeight;
+		sd.BufferDesc.RefreshRate = m_clientRefreshRate;
+		sd.BufferDesc.Format = m_backBufferFormat;
+		sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+		sd.SampleDesc.Count = m_MsaaState ? 4 : 1;
+		sd.SampleDesc.Quality = m_MsaaState ? (m_MsaaQuality - 1) : 0;
+
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.BufferCount = m_swapChainBufferCount;
+		sd.OutputWindow = m_mainWin;
+		sd.Windowed = true;
+		sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+		sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+		// Note: Swap chain uses queue to perform flush
+		ThrowIfFailed(m_dxgiFactory->CreateSwapChain(
+			m_commandQueue.Get(),
+			&sd,
+			m_swapChain.GetAddressOf()
+		));
+	}
+
+	FORCE_INLINE void DirectX12Renderer::CreateDescriptorHeaps()
+	{
+		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
+		rtvHeapDesc.NumDescriptors = m_swapChainBufferCount;
+		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		rtvHeapDesc.NodeMask = 0;
+
+		ThrowIfFailed(m_d3dDevice->CreateDescriptorHeap(
+			&rtvHeapDesc,
+			IID_PPV_ARGS(m_rtvHeap.GetAddressOf())
+		));
+
+		D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
+		dsvHeapDesc.NumDescriptors = 1;
+		dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+		dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		dsvHeapDesc.NodeMask = 0;
+
+		ThrowIfFailed(m_d3dDevice->CreateDescriptorHeap(
+			&dsvHeapDesc,
+			IID_PPV_ARGS(m_dsvHeap.GetAddressOf())
+		));
+	}
+
+	FORCE_INLINE void DirectX12Renderer::CreateRenderTargetView()
+	{
+		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+		for (uint32_t i = 0; i < m_swapChainBufferCount; i++)
+		{
+			ThrowIfFailed(m_swapChain->GetBuffer(
+				i, 
+				IID_PPV_ARGS(&m_swapChainBuffer[i])
+			));
+
+			m_d3dDevice->CreateRenderTargetView(
+				m_swapChainBuffer[i].Get(),
+				nullptr,
+				rtvHeapHandle
+			);
+
+			rtvHeapHandle.Offset(1, m_rtvDescriptorSize);
+		}
+	}
+
+	FORCE_INLINE void DirectX12Renderer::CreateDepthStencilBuffer()
+	{
+		// Create the depth/stencil buffer and view
+		D3D12_RESOURCE_DESC depthStencilDesc;
+		depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+		depthStencilDesc.Alignment = 0;
+		depthStencilDesc.Width = m_clientWidth;
+		depthStencilDesc.Height = m_clientHeight;
+		depthStencilDesc.DepthOrArraySize = 1;
+		depthStencilDesc.MipLevels = 1;
+		depthStencilDesc.Format = m_depthStencilFormat;
+		depthStencilDesc.SampleDesc.Count = m_MsaaState ? 4 : 1;
+		depthStencilDesc.SampleDesc.Quality = m_MsaaState ? (m_MsaaQuality - 1) : 0;
+		depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+		depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+		D3D12_CLEAR_VALUE optClear;
+		optClear.Format = m_depthStencilFormat;
+		optClear.DepthStencil.Depth = 1.0f;
+		optClear.DepthStencil.Stencil = 0;
+		
+		CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_DEFAULT);
+		ThrowIfFailed(m_d3dDevice->CreateCommittedResource(
+			&heapProps,
+			D3D12_HEAP_FLAG_NONE,
+			&depthStencilDesc,
+			D3D12_RESOURCE_STATE_COMMON,
+			&optClear,
+			IID_PPV_ARGS(m_depthStencilBuffer.GetAddressOf())
+		));
+
+		// Create descriptor to mip level 0 of entire resource using the format of the resource
+		m_d3dDevice->CreateDepthStencilView(
+			m_depthStencilBuffer.Get(),
+			nullptr,
+			DepthStencilView()
+		);
+
+		// Transition the resource from its initial state to state so it can be used as a depth buffer
+		auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+			m_depthStencilBuffer.Get(),
+			D3D12_RESOURCE_STATE_COMMON,
+			D3D12_RESOURCE_STATE_DEPTH_WRITE
+		);
+		m_commandList->ResourceBarrier(1, &barrier);
+	}
+
+	FORCE_INLINE void DirectX12Renderer::SetViewport()
+	{
+		D3D12_VIEWPORT vp;
+		vp.TopLeftX = 0.0f;
+		vp.TopLeftY = 0.0f;
+		vp.Width = static_cast<float>(m_clientWidth);
+		vp.Height = static_cast<float>(m_clientHeight);
+		vp.MinDepth = 0.0f;
+		vp.MaxDepth = 1.0f;
+
+		m_commandList->RSSetViewports(
+			1,
+			&vp
+		);
+	}
+
+	/*FORCE_INLINE void DirectX12Renderer::SetScissorRectangles(D3D12_RECT* rect)
+	{
+		m_commandList->RSSetScissorRects(1, rect);
+	}*/
+
+	FORCE_INLINE D3D12_CPU_DESCRIPTOR_HANDLE DirectX12Renderer::CurrentBackBufferView() const
+	{
+		return CD3DX12_CPU_DESCRIPTOR_HANDLE(
+			m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), // handle start 
+			m_currBackBuffer, // Index to offset 
+			m_rtvDescriptorSize // Size in bytes of the descriptor
+		);
+	}
+
+	FORCE_INLINE D3D12_CPU_DESCRIPTOR_HANDLE DirectX12Renderer::DepthStencilView() const
+	{
+		return m_dsvHeap->GetCPUDescriptorHandleForHeapStart();
+	}
+
+	void DirectX12Renderer::Initialize(DisplayTarget targetWindow)
+	{
+		// cache a reference to the target window
+		m_mainWin = targetWindow.ToHWND();
+
+		// Initialize DirectX components using m_mainWin
+		CreateDevice();
+		CreateFence();
+		GetDescriptorSizes();
+		CheckMSAAQualitySupport(4);
+		CreateCommandQueue();
+		CreateCommandAllocator();
+		CreateCommandList();
+		CreateSwapChain();
+		CreateDescriptorHeaps();
+		CreateRenderTargetView();
+		CreateDepthStencilBuffer();
+		SetViewport();
 	}
 }
